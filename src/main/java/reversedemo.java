55fdd1e7e808020978cc9d993b0532d28b2641b5
Pradeep Baraik <pradeepbaraik@pradeepbaraikcleargit config --global user.name Pradeep>
2026-02-05 12:24:52 +0530

    reverse

--- src/main/java/reversedemo.java
+++ src/main/java/reversedemo.java
@@ -1,14 +1,21 @@
 public class reversedemo {
     public static void main(String[] args){
         String p = "pradeep";
-        String reversed = "";
-        for (int i = p.length()-1; i>=0; i--){
-            reversed = reversed+ p.charAt(i);
-        }
-            System.out.println(reversed);
+//        String reversed = "";
+//        for (int i = p.length()-1; i>=0; i--){
+//            reversed = reversed+ p.charAt(i);
+//        }
+//            System.out.println(reversed);
+//
+//        if (p.equals(reversed)){
+//            System.out.println("It is a pallindrome");
+//        }
+//        else {
+//            System.out.println("It is not pallindrome");
+//        }
 
-//        StringBuilder sb = new StringBuilder(p);
-//        System.out.println(sb.reverse());
+        StringBuilder sb = new StringBuilder(p);
+        System.out.println(sb.reverse());
 
     }
 }
