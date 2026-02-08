c8c69917f5634e1c3d23bd697e8a83f10184b20b
Pradeep Baraik <pradeepbaraik@Pradeeps-MacBook-Air.local>
2026-02-08 11:52:57 +0530

    loop

---
+++ src/main/java/forloopexample.java
@@ -0,0 +1,25 @@
+public class forloopexample {
+
+    public static void main(String[] args){
+
+//        if (5>8)
+//        System.out.println("5 is greater");
+//
+//        else
+//            System.out.println("5 is not greater");
+
+
+        for (int i=0; i<10; i=i+2)
+        {
+            if (i==8)
+            {
+                System.out.println("8 is visible");
+            }
+            else
+                System.out.println("8 is not visible");
+
+
+        }
+
+    }
+}
