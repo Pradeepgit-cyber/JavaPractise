6e6ae2be41e95657d5dd31fa8c2d119106f984da
Pradeep Baraik <pradeepbaraik@Pradeeps-MacBook-Air.local>
2026-02-08 11:14:50 +0530

    newchanges

--- src/main/java/StringDemo.java
+++ src/main/java/StringDemo.java
@@ -1,9 +1,7 @@
 public class StringDemo {
     public static void main(String[] args){
-//        string literal
         String a = "Hello";
         String b = "Hello";
-//        a.concat("world");
         System.out.println(a);
 //        string classes
         String s = new String("hello");
@@ -14,24 +12,18 @@ public class StringDemo {
         System.out.println(s==s1);
         System.out.println(a.equalsIgnoreCase(s));
 
-//        stringbuffer and stringBuilder are mutable
         StringBuffer sb = new StringBuffer("Hello");
         sb.append("World");
         System.out.println(sb);
         sb.insert(2, "she");
         System.out.println(sb);
-//        HeshelloWorld
         sb.replace(3, 5, "aa");
         System.out.println(sb);
-//        HesaalloWorld
         sb.deleteCharAt(12);
         System.out.println(sb);
-//        HesaalloWorl
         sb.reverse();
         System.out.println(sb);
-//      lroWollaaseH
 
 
     }
-
 }
