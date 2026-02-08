acefe4703db92fb5f2a6ba5c94532889148ab9f4
Pradeep Baraik <pradeepbaraik@Pradeeps-MacBook-Air.local>
2026-02-08 13:55:56 +0530

    new

--- src/main/java/forloopexample.java
+++ src/main/java/forloopexample.java
@@ -9,15 +9,20 @@ public class forloopexample {
 //            System.out.println("5 is not greater");
 
 
-        for (int i=0; i<10; i=i+2)
+//        for (int i=0; i<10; i=i+2)
+//        {
+//            if (i==8)
+//            {
+//                System.out.println("8 is visible");
+//            }
+//            else
+//                System.out.println("8 is not visible");
+//
+//
+//        }
+        for (int i=0; i<10; i++)
         {
-            if (i==8)
-            {
-                System.out.println("8 is visible");
-            }
-            else
-                System.out.println("8 is not visible");
-
+            System.out.println(i);
 
         }
 
