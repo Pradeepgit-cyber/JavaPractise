263c0d24f496c23f322b74b06d96b299a8e5ce5e
Pradeep Baraik <pradeepbaraik@pradeepbaraikcleargit config --global user.name Pradeep>
2026-02-09 17:26:56 +0530

    NewLearning

---
+++ src/main/java/AustralianTraffic.java
@@ -0,0 +1,23 @@
+import org.example.CentralTraffic;
+
+public class AustralianTraffic implements CentralTraffic {
+
+    public static void main(String[] args){
+
+    }
+
+    @Override
+    public void greenGo() {
+
+    }
+
+    @Override
+    public void redtop() {
+
+    }
+
+    @Override
+    public void flashYello() {
+
+    }
+}
---
+++ src/main/java/org/example/CentralTraffic.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public interface CentralTraffic {
+    public void greenGo();
+    public void redtop();
+    public void flashYello();
+
+}
